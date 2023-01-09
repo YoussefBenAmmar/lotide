59b106609fad505d448c8311e7391a38544ed696
YoussefBenAmmar <benammar.y@gmail.com>
2023-01-09 14:52:16 -0500

    array tail assertion

--- head.js
+++ head.js
@@ -1,15 +1,3 @@
-const emojiAngryFace = String.fromCodePoint(0x1F621);
-const emojiFire = String.fromCodePoint(0x1F525);
-
-const assertEqual = function(actual, expected) {
-
-  if (actual === expected) {
-    console.log(`${emojiFire} Assertion Passed: ${actual} === ${expected}`);
-  } else {
-    console.log(`${emojiAngryFace} Assertion Failed: ${actual} === ${expected}`);
-  }
-};
-
 
 function head(arr) {
   
