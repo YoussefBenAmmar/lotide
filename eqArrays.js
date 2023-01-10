117e1cb51e4737a4e5439e2ad754299bb9a61cc7
YoussefBenAmmar <benammar.y@gmail.com>
2023-01-10 16:02:45 -0500

    assert equals for arrays

---
+++ eqArrays.js
@@ -0,0 +1,40 @@
+const emojiAngryFace = String.fromCodePoint(0x1F621);
+const emojiFire = String.fromCodePoint(0x1F525);
+
+const assertEqual = function(actual, expected) {
+
+  if (actual === expected) {
+    console.log(`${emojiFire} Assertion Passed: ${actual} === ${expected}`);
+  } else {
+    console.log(`${emojiAngryFace} Assertion Failed: ${actual} === ${expected}`);
+  }
+};
+
+const eqArrays = function (arr1, arr2){
+
+  if (arr1.length !== arr2.length){
+    return false;
+  }
+  for ( i = 0; i<arr1.length; i++){
+    if (arr1[i] !== arr2[i]){
+      return false
+    } else if (arr1[i] === arr2[i]){
+      return true
+    } else {
+      return false
+    }
+  }
+
+
+
+  if (arr1 === arr2){
+    return true
+  } else {
+    return false
+  }
+
+}
+
+assertEqual(eqArrays([1, 2, 3], [1, 2, 3]), true); // => should PASS
+
+
