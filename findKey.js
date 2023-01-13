f4282eff4b4296a049362b5140e3355069f912d2
YoussefBenAmmar <benammar.y@gmail.com>
2023-01-12 19:08:38 -0500

    done find key

---
+++ findKey.js
@@ -0,0 +1,33 @@
+const emojiAngryFace = String.fromCodePoint(0x1F621);
+const emojiFire = String.fromCodePoint(0x1F525);
+
+const assertEqual = function(actual, expected) {
+
+  if (actual === expected) {
+    console.log(`${emojiFire} Assertion Passed: ${actual} === ${expected}`);
+  } else {
+    console.log(`${emojiAngryFace} Assertion Failed: ${actual} !== ${expected}`);
+  }
+};
+
+
+const findKey = function(object, callback){
+  for (let key in object) {
+    if (callback(object[key])) {
+      return key;
+    }
+  }
+};
+
+
+
+const result1 = findKey({
+  "Blue Hill": { stars: 1 },
+  "Akaleri":   { stars: 3 },
+  "noma":      { stars: 2 },
+  "elBulli":   { stars: 3 },
+  "Ora":       { stars: 2 },
+  "Akelarre":  { stars: 3 }
+}, x => x.stars === 2) 
+
+assertEqual(result1, "Bootcamp");
