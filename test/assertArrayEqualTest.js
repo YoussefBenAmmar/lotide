b5f0d3c0b7cb6e9204c3105a504d2eba08826f44
YoussefBenAmmar <benammar.y@gmail.com>
2023-01-16 09:26:01 -0500

    refactored AssertArraysEqual with eqArray + assertArray.

---
+++ test/assertArrayEqualTest.js
@@ -0,0 +1,8 @@
+const assertEqual = require('../assertEqual');
+const assertArraysEqual = require ('../assertArraysEqual');
+
+
+
+assertArraysEqual([1, 2, 3], [1, 2, 3]);
+
+assertArraysEqual([1, 2, 3], [1, 5, 3]);
